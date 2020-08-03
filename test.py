d5e241a61c18b456986193274facdbb212434e73
Koushik <2Koush@gmail.com>
2020-08-03 19:57:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello 1")
+import json
+
+def lambda_handler(event, context):
+    print("Hello 1")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
